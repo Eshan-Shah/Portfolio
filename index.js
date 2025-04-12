aa8ed27d04c06b9178eeaeb1dbb17f948ed6a3bf
Eshan Shah <Eshan@Shahs-MacBook-Air.local>
2025-04-12 14:45:02 +0100

    Created base pages with a navigation bar and titles

---
+++ index.js
@@ -0,0 +1,11 @@
+function toProjects() {
+    window.location.href = "projects.html"
+}
+
+function toContacts() {
+    window.location.href = "contact.html"
+}
+
+function toAbout() {
+    window.location.href = "index.html"
+}
