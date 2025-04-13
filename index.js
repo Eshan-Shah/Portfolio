f9b0934bddc71711b6fd90305204a4dadc5f7949
Eshan Shah <Eshan@Shahs-MacBook-Air.local>
2025-04-13 16:38:57 +0100

    Vanilla routing fixed

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
-import { Analytics } from "@vercel/analytics/react"
-
 function toProjects() {
-    window.location.href = "projects.html"
-}
-
-function toContacts() {
-    window.location.href = "contact.html"
-}
-
-function toAbout() {
-    window.location.href = "index.html"
-}
-
-function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-      {/* This is where you add Vercel Analytics */}
-      <Analytics />
-    </>
-  );
-}
-
-export default MyApp;
+    window.location.href = "projects.html";
+  }
+  
+  function toContacts() {
+    window.location.href = "contact.html";
+  }
+  
+  function toAbout() {
+    window.location.href = "index.html";
+  }
+  
