d543bc554a196dfcd6ad49e7c1ae2025a9d62150
Eshan Shah <Eshan@Shahs-MacBook-Air.local>
2025-04-13 08:44:00 +0100

    Added text to the About me section

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+import { Analytics } from "@vercel/analytics/react"
+
 function toProjects() {
-    window.location.href = "projects.html"
+    window.location.href = "projects"
 }
 
 function toContacts() {
-    window.location.href = "contact.html"
+    window.location.href = "contact"
 }
 
 function toAbout() {
-    window.location.href = "index.html"
+    window.location.href = "index"
 }
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      {/* This is where you add Vercel Analytics */}
+      <Analytics />
+    </>
+  );
+}
+
+export default MyApp;
